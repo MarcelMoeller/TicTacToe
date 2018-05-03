7921f1ab3963cfcaec40e513b174875e7edfbd12
Marcel Möller <marcelmoeller99@gmx.de>
2018-05-03 08:18:47 +0200

    Add some comments to the index.php

--- index.php
+++ index.php
@@ -1,21 +1,28 @@
 <?php
     session_start();
+    //Include classes
     require_once("TicTacToe.php");
     require_once("Player.php");
     require_once("Board.php");
+    //Check if the $_SESSION variable contains the tictactoe object
     if(isset($_SESSION["Tictactoe"])){
+        //Unserialize tictactoe variable
         $game = unserialize($_SESSION["Tictactoe"]);
+        //Check if the $_GET variable is not empty
         if(!empty($_GET)){
             $key = key($_GET);
             $matches = [];
             preg_match("/cell-(\d)-(\d)/",$key,$matches);
             $row = $matches[1];
             $column = $matches[2];
+            //Play a turn with the values of the $_GET variable
             $game->playTurn($row,$column,$_GET[$key]);
         } else {
+            //If $_GET is empty just show the board
             $game->showBoard();
         }
     } else {
+        //Create a new TicTacToe game including the board and the two players and show the empty board afterwards
         $board = new Board();
         $playerOne = new Player("Marcel","X");
         $playerTwo = new Player("Marcel2","O");
@@ -24,6 +31,7 @@
         $game->showBoard();
     }
 
+    //Safe the current TicTacToe game into a session
     $_SESSION["Tictactoe"] = serialize($game);
 
 ?>
