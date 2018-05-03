621fb9131ea63246f407d8ede204496c108fa5a6
Marcel Möller <marcelmoeller99@gmx.de>
2018-05-03 09:33:36 +0200

    Add missing comments for TicTacToe class

--- TicTacToe.php
+++ TicTacToe.php
@@ -1,5 +1,6 @@
 <?php
 
+//TicTacToe class
 class TicTacToe
 {
     /** @var \Board $board */
@@ -26,6 +27,7 @@ class TicTacToe
      */
     public function __construct(Board $board, Player $playerOne, Player $playerTwo, Player $currentPlayer)
     {
+        //Assing parameter to TicTacToe properties
         $this->board = $board;
         $this->playerOne = $playerOne;
         $this->playerTwo = $playerTwo;
@@ -40,27 +42,37 @@ class TicTacToe
      */
     public function playTurn($row, $column, $symbol)
     {
+        //Set a symbol with setPosition() and check if the method returns true
+        //setPosition() method returns true if the symbol got successfully set at the specific position
         if($this->board->setPosition($row,$column,$symbol)) {
+            //Check if the game ends
             if($this->checkGameEnd()) {
+                //Check if the game ended in a draw and output the specific notification
                 if ($this->isDraw === true) {
                     echo '<script>alert("This game resulted in a draw.")</script>';
                 } else {
                     echo '<script>alert("The winner is ' . $this->currentPlayer->getName() . '")</script>';
                 }
+                //Destroy the session due to game end
+                //A new game will start afterwards
                 session_destroy();
             }
+            //Switch the player and show the current board after the current player played his turn
             $this->togglePlayer();
             $this->board->showBoard($this->currentPlayer);
 
         } else {
+            //If the symbol couldn't get set just show the board
             $this->board->showBoard($this->currentPlayer);
         }
     }
 
     /**
+     * Toggles between the two players of the TicTacToe object
      * @UMLCHANGES There is no method to toggle players and it would be better to outsource it into its on method instead of writing it into the playTurn method
      */
     private function togglePlayer(){
+        //Check if the currentPlayer is either playerOne or playerTwo and switch to the other one
         if($this->currentPlayer === $this->playerOne) {
             $this->currentPlayer = $this->playerTwo;
         } elseif ($this->currentPlayer === $this->playerTwo) {
@@ -71,6 +83,7 @@ class TicTacToe
     }
 
     /**
+     * Check if the game ends and return a boolean
      * @UMLCHANGES Rename getWinner() function and check the winner through currentPlayer
      *
      * @return bool
@@ -86,34 +99,40 @@ class TicTacToe
             /** Check for winning row */
             $checkRow = $board[$iRow];
 
-            //Check the row for a empty field
+            //Check the row for a empty field and set $gotEmptyField to true if there is one
             if(in_array("",$checkRow)) {
                 $gotEmptyField = true;
             }
 
-            /** Check for winning column*/
+            //Assign the the current column to the checkColumn variable
             $checkColumn = [];
             for ($iColumn = 0; $iColumn < count($board); $iColumn++) {
                 $checkColumn[] = $board[$iColumn][$iRow];
             }
 
-            /** Check for winning diagonal */
-            $currDiaColumn = count($board) - 1 - $iRow;
+            //Add the variable currDiaColumn to get a whole diagonal which starts at the bottom left and ends at the top right.
+            //The "-1" is important cause the count function starts counting at 1 and $iRow starts at 0. That means that there would be one column to much
+            $currDiaColumn = count($board) - $iRow  - 1;
+            //Diagonal bottom left to top right
             $checkDiaLeft[] = $board[$iRow][$currDiaColumn];
+            //Diagonal top left to bottom right
             $checkDiaRight[] = $board[$iRow][$iRow];
+            //Check if the prepared column and row only got one unique symbol inside and no empty fields
             if( $this->checkUnique($checkColumn) || $this->checkUnique($checkRow)) {
                 return true;
             }
         }
+        //Check if the prepared diagonals only got one unique symbol inside and no empty fields
         if( $this->checkUnique($checkDiaLeft) || $this->checkUnique($checkDiaRight)) {
             return true;
         }
 
-        //Set isDraw to true and return if there is no empty field
+        //Set isDraw to true and return true if there is no empty field
         if($gotEmptyField === false) {
             $this->isDraw = true;
             return true;
         }
+        //If none of the checks which would end the game return true, return false to continue the game.
         return false;
     }
 
@@ -126,14 +145,16 @@ class TicTacToe
      */
     private function checkUnique($array)
     {
+        //Remove all elements which occure atleast twice in the array.
         $array = array_unique($array);
+        //Check if the unique array got only one value and if this value is not empty
         if (count($array) === 1 && !empty($array[0])) {
             return true;
         }
     }
 
     /**
-     * Returns the board
+     * Call the showBoard method of the board
      */
     public function showBoard() {
         $this->board->showBoard($this->currentPlayer);
