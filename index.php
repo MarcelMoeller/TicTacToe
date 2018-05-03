3f40cac863a1cc458d5f82cfc00477a712965e9e
Marcel Möller <marcelmoeller99@gmx.de>
2018-05-03 10:03:09 +0200

    Change how get the key from the $_GET variable which is needed to play a turn. Now it collects all keys instead of the first one and plays a turn if pregmatch gets a match. If the pregmatch function matches it instantly breaks out of the for loop to prevent manipulation of the game.

--- index.php
+++ index.php
@@ -9,14 +9,25 @@
         //Unserialize tictactoe variable
         $game = unserialize($_SESSION["Tictactoe"]);
         //Check if the $_GET variable is not empty
-        if(!empty($_GET)){
-            $key = key($_GET);
+        if(isset($_GET) && !empty($_GET)){
+            //Get all keys of the $_GET array
+            $keys = array_keys($_GET);
             $matches = [];
-            preg_match("/cell-(\d)-(\d)/",$key,$matches);
-            $row = $matches[1];
-            $column = $matches[2];
-            //Play a turn with the values of the $_GET variable
-            $game->playTurn($row,$column,$_GET[$key]);
+            //Iterate through all keys and check if pregmatch gets a match
+            for($i = 0; $i < count($keys); $i++) {
+                preg_match("/cell-(\d)-(\d)/",$keys[$i],$matches);
+                if(!empty($matches)) {
+                    //Get the match key and the groups 1 and 2 which contains the col and the row
+                    $key = $keys[$i];
+                    $row = $matches[1];
+                    $col = $matches[2];
+                    //Play a turn with the values of the $_GET variable
+                    $game->playTurn($row,$col,$_GET[$key]);
+                    //Break out of the for loop because it is only allowed to play a single turn per player.
+                    //Without the break the game could be manipulated.
+                    break;
+                }
+            }
         } else {
             //If $_GET is empty just show the board
             $game->showBoard();
